9953cd8a9ee54de437c02c30b8ae8e55362e5c58
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-03-19 18:14:53 -0500

    Changed values

--- src/AboutYou.java
+++ src/AboutYou.java
@@ -4,10 +4,10 @@ public class AboutYou {
 	public static void main(String[] args) {
 		// TODO Modify the values in the String literals in ALL_CAPS below
 		System.out.print("Hello, my name is ");
-		System.out.println("YOUR_NAME");
+		System.out.println("Anthony, though I go by A.J.");
 
 		System.out.print("And I'm taking this class because ");
-		System.out.println("BECAUSE_WHY");
+		System.out.println("I wasn't quite profficient enough last semester, so I'm giving it another go!");
 	}
 
 }
